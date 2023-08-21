02fde38810a64dbf15c93244ac7ce1d187830e02
SzyJar <107247457+SzyJar@users.noreply.github.com>
2023-08-22 00:22:35 +0200

    Response change in RPS

--- src/controllers/rps/response.js
+++ src/controllers/rps/response.js
@@ -1,31 +1,29 @@
 export function makeResponse(winrate, history) {
 
-  if (winrate > 55 && history > 12) {
+  // Player is winning
+  if (winrate > 53 && history > 12) {
     return 'I am impressed that you managed to get a positive win rate against me!'
-  };
-
-  if (winrate < 30 && history > 15 && history < 99) {
-    return 'You are predictable.'
   };
 
   if (winrate > 63 && history > 22) {
     return 'It\'s IMPOSSIBLE! How are you able to win so many rounds?'
   };
 
-  if (winrate < 24 && winrate > 10 && history > 35) {
-    return 'It\'s too easy to beat you, you\'re not even trying, are you?'
-  };
-
-  if (winrate < 12 && history > 55) {
-    return 'It\'s pointless, you\'re not going anywhere with your strategy.'
-  };
-
-  if (winrate > 35 && winrate < 58 && history > 100) {
+  // Player is losing
+  if (winrate > 35 && winrate < 51 && history > 60) {
     return 'You are persistent. But the longer you play, the harder it gets.'
   };
 
-  if (winrate > 27 && winrate < 33 && history > 100) {
-    return 'Mhhh.... Interesting.'
+  if (winrate < 45 && winrate > 22 && history > 15) {
+    return 'You are becoming predictable.'
+  };
+
+  if (winrate < 19 && winrate > 12 && history > 15) {
+    return 'It\'s too easy to beat you, you\'re not even trying, are you?'
+  };
+
+  if (winrate < 10 && history > 15) {
+    return 'It\'s pointless, you\'re not going anywhere with your strategy.'
   };
 
   return false;
